2aa7124243caa9995f0566b18fe71b67a43f6ab9
Rushi1618 <31608167+Rushi1618@users.noreply.github.com>
2020-12-13 15:42:16 +0530

    make it better

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-	path: '/peerjs',
-	host: '/',
-	port: '443'
+  path: '/peerjs',
+  host: '/',
+  port: '3000'
 })
-let myVideoStream;
+let myVideoStream
 const myVideo = document.createElement('video')
-myVideo.muted = true;
+myVideo.muted = true
 const peers = {}
 navigator.mediaDevices.getUserMedia({
 	video: true,
@@ -16,7 +16,9 @@ navigator.mediaDevices.getUserMedia({
 	myVideoStream = stream;
 	addVideoStream(myVideo, stream)
 	myPeer.on('call', call => {
+		console.log("Before answer: " + call)
 		call.answer(stream)
+		console.log("After answer: " + call)
 		const video = document.createElement('video')
 		call.on('stream', userVideoStream => {
 			addVideoStream(video, userVideoStream)
@@ -30,10 +32,10 @@ navigator.mediaDevices.getUserMedia({
 	let text = $("input");
 	// when press enter send message
 	$('html').keydown(function (e) {
-		if (e.which == 13 && text.val().length !== 0) {
-			socket.emit('message', text.val());
-			text.val('')
-		}
+	if (e.which == 13 && text.val().length !== 0) {
+		socket.emit('message', text.val());
+		text.val('')
+	}
 	});
 	socket.on("createMessage", message => {
 		$("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
@@ -50,10 +52,12 @@ myPeer.on('open', id => {
 })
 
 function connectToNewUser(userId, stream) {
+    console.log("User connected: " + userId)
 	const call = myPeer.call(userId, stream)
 	const video = document.createElement('video')
-	call.on('stream', userVideoStream => {
+	call.on('stream', (userVideoStream) => {
 		addVideoStream(video, userVideoStream)
+		console.log("connected: " + userId)
 	})
 	call.on('close', () => {
 		video.remove()
@@ -73,62 +77,62 @@ function addVideoStream(video, stream) {
 
 
 const scrollToBottom = () => {
-	var d = $('.main__chat_window');
-	d.scrollTop(d.prop("scrollHeight"));
+  var d = $('.main__chat_window');
+  d.scrollTop(d.prop("scrollHeight"));
 }
 
 
 const muteUnmute = () => {
-	const enabled = myVideoStream.getAudioTracks()[0].enabled;
-	if (enabled) {
-		myVideoStream.getAudioTracks()[0].enabled = false;
-		setUnmuteButton();
-	} else {
-		setMuteButton();
-		myVideoStream.getAudioTracks()[0].enabled = true;
-	}
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
 }
 
 const playStop = () => {
-	console.log('object')
-	let enabled = myVideoStream.getVideoTracks()[0].enabled;
-	if (enabled) {
-		myVideoStream.getVideoTracks()[0].enabled = false;
-		setPlayVideo()
-	} else {
-		setStopVideo()
-		myVideoStream.getVideoTracks()[0].enabled = true;
-	}
+  console.log('object')
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo()
+  } else {
+    setStopVideo()
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
 }
 
 const setMuteButton = () => {
-	const html = `
+  const html = `
     <i class="fas fa-microphone"></i>
     <span>Mute</span>
   `
-	document.querySelector('.main__mute_button').innerHTML = html;
+  document.querySelector('.main__mute_button').innerHTML = html;
 }
 
 const setUnmuteButton = () => {
-	const html = `
+  const html = `
     <i class="unmute fas fa-microphone-slash"></i>
     <span>Unmute</span>
   `
-	document.querySelector('.main__mute_button').innerHTML = html;
+  document.querySelector('.main__mute_button').innerHTML = html;
 }
 
 const setStopVideo = () => {
-	const html = `
+  const html = `
     <i class="fas fa-video"></i>
     <span>Stop Video</span>
   `
-	document.querySelector('.main__video_button').innerHTML = html;
+  document.querySelector('.main__video_button').innerHTML = html;
 }
 
 const setPlayVideo = () => {
-	const html = `
+  const html = `
   <i class="stop fas fa-video-slash"></i>
     <span>Play Video</span>
   `
-	document.querySelector('.main__video_button').innerHTML = html;
+  document.querySelector('.main__video_button').innerHTML = html;
 }
